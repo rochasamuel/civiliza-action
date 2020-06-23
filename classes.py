45241b08eff67aa70575bd35f16e409c99f21e69
Samuel Rocha <samuelsilv.rocha@gmail.com>
2020-06-23 19:50:57 -0300

    Esqueleto de classes

---
+++ classes.py
@@ -0,0 +1,72 @@
+##############################################################
+class Mundo():
+    pass
+
+    def testebranch():
+        pass
+        
+##############################################################
+class Panorama():
+    pass
+
+    def testePanorama():
+        pass
+
+##############################################################
+class Pais():
+    pass
+        
+##############################################################
+class setorEconomico():
+    pass
+        
+##############################################################
+class setorMilitar():
+    pass
+        
+##############################################################
+class setorPrivado():
+    pass    
+    
+##############################################################
+class Lider():
+    pass    
+
+##############################################################
+class Acao():
+   pass
+
+    def fazerEfeito(self):
+        pass        
+
+##############################################################
+class InteracaoFixa():
+    pass
+
+    def fazerEfeito(self):
+        pass
+    
+##############################################################
+class InteracaoMilitar(InteracaoFixa):
+    pass
+        
+    def fazerEfeito(self):
+        pass        
+
+##############################################################
+class InteracaoEconomica(InteracaoFixa):
+    pass
+
+    def fazerEfeito(self):
+        pass
+
+##############################################################
+class InteracaoPrivada(InteracaoFixa):
+    pass
+
+    def fazerEfeito(self):
+        pass
+
+##############################################################
+class Jogador(Pais):
+    pass
