4f02caf5a0208d31ac60d962581a35ec5ad39504
Samuel Rocha <samuelsilv.rocha@gmail.com>
2020-06-23 19:59:30 -0300

    Adicionado atributos e métodos da classe Mundo

--- classes.py
+++ classes.py
@@ -1,9 +1,43 @@
 ##############################################################
 class Mundo():
-    pass
+    """
+    Classe onde todos os outros objetos vão residir. Vai ser usada majoritariamente como um hub de informações.
+    """
+    def __init__(self):
+        """
+        A classe é instanciada sem nenhum atributo porque eles são instaciados depois, recebendo Mundo() como attr.
+        """
+        self.paises = {} # é um dicionário no formato {'nome_pais':Pais()}
+        self.ano = 1 # Variável de contagem de turnos
+        self.interacoesFixas = [] # Lista com os objetos interacaoFixa()
+        self.panorama = None # Panorama() do mundo, com os três filhos
 
     def testebranch():
         pass
+    def paisesAleatorios(self, n_paises=None):
+        """
+        Método a ser usado para gerar interacaoFixa() aleatórias.
+        :param n_paises: (opcional) Numero preciso de países contidos. Se omitido, escolhe-se um número aleatório
+        entre 2 e o número de países.
+        :return: Dicionário no formato {'nome_pais':Pais()} contendo n_paises chaves.
+        """
+        if len(self.paises) <= 2:
+            # Exceção para caso esteja vazio ou com menos países do que o necessário
+            raise Exception('O mundo nem país tem. Resolve isso primeiro.')
+
+        if n_paises is None:
+            n_paises = np.random.randint(2, len(self.paises))
+
+        nomesAleatorios = set()
+
+        while len(nomesAleatorios) < n_paises:
+            nomesAleatorios.add(random.choice(self.paises.keys()))
+
+        nomesAleatorios = sorted(list(nomesAleatorios))
+
+        nomesAleatorios = {pais: self.paises[pais] for pais in nomesAleatorios}
+
+        return nomesAleatorios
         
 ##############################################################
 class Panorama():
