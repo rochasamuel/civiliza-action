2c88451727614d08620e0d8852945625cdb0ce51
Samuel Rocha <samuelsilv.rocha@gmail.com>
2020-06-23 20:28:57 -0300

    Esqueleto dos instanciadores

---
+++ instanciadores.py
@@ -0,0 +1,20 @@
+def random_string_generator(str_size):
+    pass
+
+def createSetorEconomico():
+    pass
+
+def createSetorMilitar():
+    pass
+
+def createSetorPrivado():
+    pass
+
+def createLider():
+    pass
+
+def createPais(nome, mundo):
+    pass
+   
+def createInteracaoFixa(mundo):
+    pass
