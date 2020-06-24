9ca32b424bf7ccfb9c404974eb1b039cbaceba06
Samuel Rocha <samuelsilv.rocha@gmail.com>
2020-06-24 00:13:42 -0300

    gitflow-feature-stash: fix_pull2.0

--- menu.py
+++ menu.py
@@ -1,83 +1,4 @@
-import jogo
-
-nome = str(input('Qual o seu nome?'))
-#criacao jogador
-
-opcao = 0
-
-while opcao != 9: 
-    print('''
-    [1] - Ações
-    [2] - Ver Panorama
-    [3] - Passar o Turno
-    [9] - Terminar Jogo
-    ''')
-
-    opcao = int(input('O que você quer fazer?\n'))
-
-    if opcao == 1:
-        opcao_acao = 0
-
-        while opcao_acao != 9:
-            print('''
-            [1] - Setor Economico
-            [2] - Setor Militar
-            [3] - Setor Privado
-            [9] - Retornar
-            ''')
-            
-            opcao_acao = int(input('Maneiro! Mas em qual setor?'))
-
-            if opcao_acao == 1:
-                sub_opcao_acao = 0
-
-                while sub_opcao_acao != 9:
-                    print('''
-                    [1] - Ação 1
-                    [2] - Ação 2
-                    [9] - Retornar
-                    ''')
-
-                    sub_opcao_acao = int(input('Top! E qual ação?'))
-
-                    if sub_opcao_acao == 1:
-                        #fazer ação
-                        break
-                    
-                    if sub_opcao_acao == 2:
-                        #fazer ação
-                        break
-                    
-                    if sub_opcao_acao == 9:
-                        break
-                continue
-
-            if opcao_acao == 2:
-                sub_opcao_acao = 0
-
-                while sub_opcao_acao != 9:
-                    print('''
-                    [1] - Ação 1
-                    [2] - Ação 2
-                    [9] - Retornar
-                    ''')
-
-                    sub_opcao_acao = int(input('Top! E qual ação?'))
-
-                    if sub_opcao_acao == 1:
-                        #fazer ação
-                        pass
-
-                    if sub_opcao_acao == 2:
-                        #fazer ação
-                        pass
-
-                    if sub_opcao_acao == 9:
-                        break
-                break
-=======
 def menuPrincipal():
->>>>>>> Stashed changes
 
     nome = str(input('Qual o seu nome?'))
     #criacao jogador
@@ -101,7 +22,7 @@ def menuPrincipal():
             if cont_opcoes >= 1:
                 print('saihdjlksahdnkas')
                 break
-<<<<<<< Updated upstream
+
 
     if opcao == 2:
         #Mostrar panorama
@@ -114,7 +35,6 @@ def menuPrincipal():
     if opcao == 9:
         #Sair
         break
-=======
             
             while opcao_acao != 9:
                 print('''
@@ -220,4 +140,4 @@ def menuPrincipal():
             break
 
 menuPrincipal()
->>>>>>> Stashed changes
+
